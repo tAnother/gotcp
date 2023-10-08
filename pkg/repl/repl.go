eff9f381dd4c17dac4734c52580d5f6fd9e625b4
Nora Tang <jingyu_tang@brown.edu>
2023-10-08 21:45:00 +0000

    [WIP] lr, send in construction.

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"iptcp-nora-yu/pkg/ipnode"
+	"log"
 	"os"
 	"strings"
 )
@@ -52,6 +53,24 @@ func (r *REPL) Run(node *ipnode.Node) {
 	replConfig := &REPLConfig{writer: writer, node: node}
 
 	// add default commands here
+	r.AddCommand("ln", func(s string, r *REPLConfig) error {
+		return lnHandler(s, replConfig)
+	}, "Prints neighbors of the current node. usage: ln")
+	r.AddCommand("li", func(s string, r *REPLConfig) error {
+		return liHandler(s, replConfig)
+	}, "Prints all interfaces of the current node. usage: li")
+	r.AddCommand("lr", func(s string, r *REPLConfig) error {
+		return lrHandler(s, replConfig)
+	}, "Prints the routing table of the current node. usage: lr")
+	r.AddCommand("up", func(s string, r *REPLConfig) error {
+		return upHandler(s, replConfig)
+	}, "Sets the interface's state to up. usage: up")
+	r.AddCommand("down", func(s string, r *REPLConfig) error {
+		return downHandler(s, replConfig)
+	}, "Sets the interface's state to down. usage: down")
+	r.AddCommand("send", func(s string, r *REPLConfig) error {
+		return sendHandler(s, replConfig)
+	}, "Sends the messgae to the ip address. usage: send")
 
 	// begin the repl
 	io.WriteString(writer, ">") // the prompt
@@ -78,22 +97,97 @@ func (r *REPL) Run(node *ipnode.Node) {
 }
 
 // ---------- some shared handler (might wanna move these elsewhere)
-func lnHandler(input string, replConfig *REPLConfig) {
+func lnHandler(input string, replConfig *REPLConfig) error {
+	args := strings.Split(input, " ")
+	if len(args) != 1 {
+		return fmt.Errorf("usage: ln")
+	}
+	neighbors := replConfig.node.GetNeighborsString()
+	rowSize := len(neighbors)
+	writer := replConfig.writer
+
+	bytes, err := io.WriteString(writer, "Iface          VIP          UDPAddr\n")
+	log.Printf("[lnHandler] writes %s bytes", bytes)
+	if err != nil {
+		return fmt.Errorf("lnHandler cannot write the header to stdout.\n")
+	}
+
+	for i := 0; i < rowSize; i++ {
+		row := neighbors[i]
+		bytes, err := io.WriteString(writer, fmt.Sprintf("%s     %s   %s\n", row[0], row[1], row[2]))
+		log.Printf("[lnHandler] writes %s bytes", bytes)
+		if err != nil {
+			return fmt.Errorf("lnHandler cannot write neighors to stdout.\n")
+		}
+	}
+	return nil
+}
+
+func liHandler(input string, replConfig *REPLConfig) error {
+	args := strings.Split(input, " ")
+	if len(args) != 1 {
+		return fmt.Errorf("usage: li")
+	}
+	interfaces := replConfig.node.GetInterfacesString()
+
+	writer := replConfig.writer
+	bytes, err := io.WriteString(writer, "Name  Addr/Prefix State\n")
+	log.Printf("[liHandler] writes %s bytes", bytes)
+	if err != nil {
+		return fmt.Errorf("liHandler cannot write the header to stdout.\n")
+	}
+
+	for i := 0; i < len(interfaces); i++ {
+		row := interfaces[i]
+		bytes, err := io.WriteString(writer, fmt.Sprintf("%s     %s   %s\n", row[0], row[1], row[2]))
+		log.Printf("[liHandler] writes %s bytes", bytes)
+		if err != nil {
+			return fmt.Errorf("liHandler cannot write interfaces to stdout.\n")
+		}
+	}
+	return nil
+}
+
+func lrHandler(input string, replConfig *REPLConfig) error {
+	args := strings.Split(input, " ")
+	if len(args) != 1 {
+		return fmt.Errorf("usage: lr")
+	}
 
 }
 
-func liHandler(input string, replConfig *REPLConfig) {
+func upHandler(input string, replConfig *REPLConfig) error {
+	args := strings.Split(input, " ")
+	if len(args) != 2 {
+		return fmt.Errorf("usage: up <interface name>")
+	}
+	interfaceName := args[1]
 
+	err := replConfig.node.SetInterfaceIsDown(interfaceName, false)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func upHandler(input string, replConfig *REPLConfig) {
+func downHandler(input string, replConfig *REPLConfig) error {
+	args := strings.Split(input, " ")
+	if len(args) != 2 {
+		return fmt.Errorf("usage: down <interface name>")
+	}
+	interfaceName := args[1]
 
+	err := replConfig.node.SetInterfaceIsDown(interfaceName, true)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func downHandler(input string, replConfig *REPLConfig) {
-
-}
-
-func sendHandler(input string, replConfig *REPLConfig) {
+func sendHandler(input string, replConfig *REPLConfig) error {
+	args := strings.Split(input, " ")
+	if len(args) != 3 {
+		return fmt.Errorf("usage: send <dest IP> <msg>")
+	}
 
 }
